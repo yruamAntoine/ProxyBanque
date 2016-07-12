81b5af81d410916ef5536e75a64292070ca05974
inti0302 <inti0302@INTI-0302>
2016-07-12 17:48:30 +0200

    compte

--- ProxyBanque1/src/main/java/fr/adaming/model/Compte.java
+++ ProxyBanque1/src/main/java/fr/adaming/model/Compte.java
@@ -1,9 +1,92 @@
 package fr.adaming.model;
 
-public class Compte {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+@Entity
+public abstract class Compte implements Serializable{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private int id;
 	private double solde;
+	private Client client;
+	
+	// Constructeurs
+	
+	/**
+	 * Constructeur vide
+	 */
+	public Compte() {
+		super();
+	}
+	
+	/**
+	 * Constructeur sans id
+	 * @param solde
+	 * @param client
+	 */
+	public Compte(double solde, Client client) {
+		super();
+		this.solde = solde;
+		this.client = client;
+	}
+	
+	/**
+	 * Constructeur full
+	 * @param id
+	 * @param solde
+	 * @param client
+	 */
+	public Compte(int id, double solde, Client client) {
+		super();
+		this.id = id;
+		this.solde = solde;
+		this.client = client;
+	}
+	
+	//Getters et Setters
+	
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the solde
+	 */
+	public double getSolde() {
+		return solde;
+	}
+	/**
+	 * @param solde the solde to set
+	 */
+	public void setSolde(double solde) {
+		this.solde = solde;
+	}
+	/**
+	 * @return the client
+	 */
+	public Client getClient() {
+		return client;
+	}
+	/**
+	 * @param client the client to set
+	 */
+	public void setClient(Client client) {
+		this.client = client;
+	}
+	
 	
 
 }
