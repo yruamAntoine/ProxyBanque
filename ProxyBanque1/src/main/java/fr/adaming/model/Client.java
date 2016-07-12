9a15e74c093e2e1254b0c207aaa45f300f0cae33
Amaury <adusser@free.fr>
2016-07-12 17:38:23 +0200

    Creation Client

--- ProxyBanque1/src/main/java/fr/adaming/model/Client.java
+++ ProxyBanque1/src/main/java/fr/adaming/model/Client.java
@@ -1,5 +1,145 @@
 package fr.adaming.model;
 
-public class Client {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clients")
+public class Client implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5331020441313093036L;
+	
+	/**
+	 * Déclaration des attributs
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id_client;
+	private String nom;
+	private String prenom;
+	private String adresse;
+	private int codePostal;
+	private String ville;
+	private String tel;
+
+	/**
+	 * Getters & setters
+	 * 
+	 * @return
+	 */
+	public int getId_client() {
+		return id_client;
+	}
+
+	public void setId_client(int id_client) {
+		this.id_client = id_client;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public int getCodePostal() {
+		return codePostal;
+	}
+
+	public void setCodePostal(int codePostal) {
+		this.codePostal = codePostal;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	/**
+	 * Constructeur plein
+	 * 
+	 * @param id_client
+	 * @param nom
+	 * @param prenom
+	 * @param adresse
+	 * @param codePostal
+	 * @param ville
+	 * @param tel
+	 */
+	public Client(int id_client, String nom, String prenom, String adresse,
+			int codePostal, String ville, String tel) {
+		super();
+		this.id_client = id_client;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.adresse = adresse;
+		this.codePostal = codePostal;
+		this.ville = ville;
+		this.tel = tel;
+	}
+
+	/**
+	 * Constructeur sans id
+	 * 
+	 * @param nom
+	 * @param prenom
+	 * @param adresse
+	 * @param codePostal
+	 * @param ville
+	 * @param tel
+	 */
+	public Client(String nom, String prenom, String adresse, int codePostal,
+			String ville, String tel) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.adresse = adresse;
+		this.codePostal = codePostal;
+		this.ville = ville;
+		this.tel = tel;
+	}
+
+	/**
+	 * Constructeur vide
+	 */
+	public Client() {
+		super();
+	}
 
 }
